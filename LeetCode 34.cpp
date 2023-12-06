309e4ad1ea9945237172a0a36718a81731b735f4
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-06 17:28:38 +0530

    First and Last Position of Element in Sorted Array

---
+++ LeetCode 34.cpp
@@ -0,0 +1,42 @@
+// LeetCode 34.Find First and Last Position of Element in Sorted Array
+//  Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+//  If target is not found in the array, return [-1, -1].
+//  You must write an algorithm with O(log n) runtime complexity.
+//  class Solution {
+//  public:
+
+// int firstOcc(vector<int>&nums, int target){
+//     int n=nums.size() , low=0, high=n-1;
+//     while(low <= high){
+//         int mid = (low + high)/2;
+//         if(nums[mid] > target)high = mid - 1;
+//         else if(nums[mid] < target)low = mid + 1;
+//         else
+//         {
+//             if(mid == 0 || nums[mid-1]!=nums[mid])return mid;
+//             else
+//             high = mid - 1;
+//         }
+//     }
+//     return -1;
+// }
+// int lastOcc(vector<int>&nums, int target){
+//     int n=nums.size() , low=0, high=n-1;
+//     while(low <= high){
+//         int mid = (low + high)/2;
+//         if(nums[mid] > target)high = mid - 1;
+//         else if(nums[mid] < target)low = mid + 1;
+//         else
+//         {
+//             if(mid == n-1 || nums[mid+1]!=nums[mid])return mid;
+//             else
+//             low = mid + 1;
+//         }
+//     }
+//     return -1;
+// }
+
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         return {firstOcc(nums , target) , lastOcc(nums , target)};
+//     }
+// };

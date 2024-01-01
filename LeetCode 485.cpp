12033dcf54f6bf17885e8408c739f3ffba40d97d
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-01-01 17:49:26 +0530

    LeetCode - 485. Max Consecutive Ones

---
+++ LeetCode 485.cpp
@@ -0,0 +1,25 @@
+// LeetCode - 485. Max Consecutive Ones
+
+//     class Solution
+// {
+// public:
+//     int findMaxConsecutiveOnes(vector<int> &nums)
+//     {
+//         int maxCount = 0;
+//         int curCount = 0;
+
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             if (nums[i] == 1)
+//             {
+//                 curCount++;
+//                 maxCount = max(maxCount, curCount);
+//             }
+//             else
+//             {
+//                 curCount = 0;
+//             }
+//         }
+//         return maxCount;
+//     }
+// };

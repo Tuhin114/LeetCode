cc0321ea4413f34284c82d1863e068b84e3d16c5
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-01-12 19:37:16 +0530

    LeetCode-74. Search a 2D Matrix

---
+++ LeetCode 74.cpp
@@ -0,0 +1,22 @@
+// LeetCode-74. Search a 2D Matrix
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+//         int row = 0, col = m - 1;
+
+//         //Traverse the matrix from (0, m-1):
+//         while(row < n && col >= 0){
+//             if(matrix[row][col] == target){
+//                 return true;
+//             }else if(matrix[row][col] < target){
+//                 row++;
+//             }else{
+//                 col--;
+//             }
+//         }
+//         return false;
+//     }
+// };

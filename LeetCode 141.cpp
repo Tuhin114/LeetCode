2ea3a69b30ca044e74036d62539a63ea3178925a
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-05 15:38:14 +0530

    LeetCode 141. Linked List Cycle

---
+++ LeetCode 141.cpp
@@ -0,0 +1,30 @@
+// LeetCode 141. Linked List Cycle
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         ListNode* fast = head;
+//         ListNode* slow = head;
+
+//         while(fast != NULL && fast -> next != NULL){
+//             fast = fast -> next;
+//             if(fast != NULL){
+//                 fast = fast -> next;
+//             }
+//             slow = slow -> next;
+
+//             if(fast == slow){
+//                 return 1;
+//             }
+//         }
+//         return 0;
+//     }
+// };

55758f8285d6624262f05f5137da251f2ed77ac7
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-27 10:31:55 +0530

    LeetCode 189. Rotate Array

---
+++ LeetCode 189.cpp
@@ -0,0 +1,17 @@
+// LeetCode 189. Rotate Array
+
+// Approach 1
+/**
+ class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+
+        vector<int> temp(nums.size());
+        for(int i = 0; i<nums.size(); i++){
+            temp[(i+k)%nums.size()] = nums[i];
+        }
+        //Copy temp into num vector
+        nums = temp;
+    }
+};
+*/

c2bef5ef03b6754a194e244cda93f6c355dfd194
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-03-14 18:55:47 +0530

    LeetCode 930. Binary Subarrays With Sum

--- LeetCode 930.cpp
+++ LeetCode 930.cpp
@@ -27,3 +27,64 @@ public:
     }
 };
 */
+
+// Optimal approach 1(Hash map)
+/**
+ class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        unordered_map<int, int> mpp;
+        int result = 0;
+        int currSum = 0;
+        mpp[0] = 1;
+
+        for(int &num : nums){
+            currSum += num;
+            int remaining_sum = currSum - goal;
+            if(mpp.find(remaining_sum) != mpp.end()){
+                result += mpp[remaining_sum];
+            }
+            mpp[currSum]++;
+        }
+        return result;
+    }
+};
+*/
+
+// Optimal approach 2(Sliding Window)
+/**
+ class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+
+        int count = 0;
+        int windowSum = 0;
+        int countZero = 0;
+
+        while(j < n){
+            windowSum += nums[j];
+
+            while(i < j && (nums[i] == 0 || windowSum > goal)){
+                if(nums[i] == 0){
+                    countZero++;
+                }else{
+                    countZero = 0;
+                }
+
+                windowSum -= nums[i];
+                i++;
+            }
+
+            if(windowSum == goal){
+                count += 1 + countZero;
+            }
+            j++;
+        }
+        return count;
+    }
+};
+*/

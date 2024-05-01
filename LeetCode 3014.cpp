fbe404a8abdd70fe0ec13d74f26884ad5a014a71
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-05-01 16:01:05 +0530

    LeetCode 3014. Minimum Number of Pushes to Type Word I

--- LeetCode 3014.cpp
+++ LeetCode 3014.cpp
@@ -26,3 +26,28 @@ public:
     }
 };
 */
+
+// Approach - 2
+/**
+ class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> mp(26, 0);
+
+        for(char &ch : word){
+            mp[ch - 'a']++;
+        }
+
+        sort(begin(mp), end(mp), greater<int>()); //Decending order of freq
+
+        int result = 0;
+
+        for(int i = 0; i < 26; i++){
+            int freq = mp[i];
+            int press = i / 8 + 1;
+            result += press * freq;
+        }
+        return result;
+    }
+};
+*/

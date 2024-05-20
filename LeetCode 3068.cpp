99d11bb0c2c19ea8f47ee890040396b2621d0bc3
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-05-20 08:12:39 +0530

    LeetCode - 3068. Find the Maximum Sum of Node Values

---
+++ LeetCode 3068.cpp
@@ -0,0 +1,32 @@
+// LeetCode - 3068. Find the Maximum Sum of Node Values
+// Hard
+// https://www.youtube.com/watch?v=QIiQdsVvjNw&list=RDCMUCaw58edcO3ZqMw76Bvs0kGQ&start_radio=1
+/**
+ class Solution {
+public:
+    typedef long long ll;
+
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        ll  sum        = 0;
+        int count      = 0;
+        int minNukasan = INT_MAX;
+
+        for (ll num : nums) {
+
+            if((num^k) > num) {
+                count++;
+                sum += (num^k);
+            } else {
+                sum += num;
+            }
+
+            minNukasan = min((long long)minNukasan, abs(num - (num ^ k)));
+        }
+
+        if(count%2 == 0)
+            return sum;
+
+        return sum - minNukasan;
+    }
+};
+*/

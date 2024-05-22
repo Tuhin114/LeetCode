0a206636ad16fbddf46aa40172a8a09c752f84e5
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-05-22 05:57:34 +0530

    LeetCode 1423. Maximum Points You Can Obtain from Cards

---
+++ LeetCode 1423.cpp
@@ -0,0 +1,27 @@
+// LeetCode 1423. Maximum Points You Can Obtain from Cards
+
+/**
+ class Solution {
+public:
+    int maxScore(vector<int>& nums, int k) {
+        int leftSum = 0,rightSum = 0,maxSum = 0;
+
+        for(int i = 0; i < k; i++){
+            leftSum += nums[i];
+        }
+
+        maxSum = leftSum;
+
+        int rightIndex = nums.size()-1;
+
+        for(int i = k-1; i >= 0; i--){
+            leftSum -= nums[i];
+            rightSum += nums[rightIndex];
+            rightIndex--;
+            maxSum=max(maxSum,leftSum+rightSum);
+        }
+
+        return maxSum;
+    }
+};a
+*/

26ede4138f31f7a2ce0b78ca4689f1bfa68ef15d
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-05-28 17:58:40 +0530

    LeetCode 145. Binary Tree Postorder Traversal

---
+++ LeetCode 145.cpp
@@ -0,0 +1,20 @@
+// LeetCode 145. Binary Tree Postorder Traversal
+
+/**
+ class Solution {
+public:
+    void postorder(TreeNode* root, vector<int>& result) {
+        if (root == nullptr) return;
+
+        postorder(root->left, result);
+        postorder(root->right, result);
+        result.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        postorder(root, result);
+        return result;
+    }
+};
+*/

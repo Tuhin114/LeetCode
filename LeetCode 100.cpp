87400be159334d457bc25cc644b352513393f1dd
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-06-04 07:11:42 +0530

    LeetCode 100. Same Tree

---
+++ LeetCode 100.cpp
@@ -0,0 +1,15 @@
+// LeetCode 100. Same Tree
+
+/**
+ class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL || q == NULL){
+            return (p == q);
+        }
+        return (p -> val == q -> val)
+                && isSameTree(p -> left, q -> left)
+                && isSameTree(p -> right, q -> right);
+    }
+};
+*/

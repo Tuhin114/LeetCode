4debc00769fd34ecb5575823508d29bb998d1e03
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-06-08 11:52:05 +0530

    LeetCode 523. Continuous Subarray Sum

---
+++ LeetCode 523.cpp
@@ -0,0 +1,28 @@
+// LeetCode 523. Continuous Subarray Sum
+//  https://www.youtube.com/watch?v=wmn3c1mP0pY
+
+/**
+ class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        unordered_map<int, int> mp;
+
+        int sum = 0;
+        mp[0] = -1;
+
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            int remainder = sum % k;
+
+            //check if its present in map
+            if(mp.find(remainder) != mp.end()){
+                if(i - mp[remainder] >= 2)  return true;
+            }else   mp[remainder] = i;
+
+        }
+        return false;
+    }
+};
+ */

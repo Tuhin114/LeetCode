c18c736a6bd4303c12bb60a46aa758f2eaacba4a
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-06-09 14:10:03 +0530

    LeetCode 974. Subarray Sums Divisible by K

---
+++ LeetCode 974.cpp
@@ -0,0 +1,81 @@
+// LeetCode 974. Subarray Sums Divisible by K
+// https://www.youtube.com/watch?v=7Xeorb721LQ
+
+// Approach - 1
+/**
+ class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n       = nums.size();
+        int count   = 0;
+        for(int i = 1; i<n; i++) {
+            nums[i] += nums[i-1];
+        }
+        for(int i = 0; i<n; i++) {
+            for(int j = i; j<n; j++) {
+                int sum = (i==0) ? nums[j] : nums[j] - nums[i-1];
+
+                if(sum%k == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+ */
+
+// Approach - 2
+/*
+    Concept behind this method
+    K
+
+    a  b  c  d   e   f   g    h
+    -----s1-------
+    -----------------s2--------
+
+    Lets say,
+    s1%k = x
+    s2%k = x
+    s1 = k*n + x
+    s2 = k*m + x
+    subarraySum = s2-s1
+                = k*(m-n)
+                Which proves that the subarray is divisible by k
+*/
+
+/**
+ class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        unordered_map<int, int> mp;
+        int sum = 0;
+
+        mp[0] = 1;
+
+        int result = 0;
+
+        for(int i = 0; i<n; i++) {
+            sum += nums[i];
+
+            int rem = sum%k;
+
+            if(rem < 0) {
+                rem += k;
+            }
+
+            if(mp.find(rem) != mp.end()) {
+                result += mp[rem];
+            }
+
+            mp[rem]++;
+
+        }
+
+        return result;
+
+    }
+};
+ */

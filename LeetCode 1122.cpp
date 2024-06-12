386e6992109bd86c1b4de2d3d9421db29fcd7c95
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-06-12 07:44:16 +0530

    leetCode 1122. Relative Sort Array

--- LeetCode 1122.cpp
+++ LeetCode 1122.cpp
@@ -1,4 +1,5 @@
 // leetCode 1122. Relative Sort Array
+// https://www.youtube.com/watch?v=Yz202siZDHA&t=582s
 
 // Approach-1 (Using counting sort)
 /**
@@ -29,4 +30,33 @@ public:
         return arr1;
     }
 };
+ */
+
+// Approach-2 (Using lambda)
+
+/**
+ vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < arr2.size(); i++) {
+            mp[arr2[i]] = i;
+        }
+
+        for(int &num : arr1) {
+            if(!mp.count(num)) {
+                mp[num] = 1e9;
+            }
+        }
+
+        auto lambda = [&](int &num1, int &num2){
+            if(mp[num1] == mp[num2]) { //1e9
+                return num1 < num2;
+            }
+
+            return mp[num1] < mp[num2];
+        };
+
+        sort(begin(arr1), end(arr1), lambda);
+
+        return arr1;
  */

d2a529a4418ea3a389d9afaf44300b8c28c51669
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-07-03 22:04:19 +0530

    LeetCode 350. Intersection of Two Arrays II

---
+++ LeetCode 350.cpp
@@ -0,0 +1,26 @@
+// LeetCode 350. Intersection of Two Arrays II
+
+/**
+ class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> countMap;
+        vector<int> result;
+
+        // Count the occurrences of each element in nums1
+        for(int num : nums1) {
+            countMap[num]++;
+        }
+
+        // Find the intersection with nums2
+        for(int num : nums2) {
+            if(countMap[num] > 0) {
+                result.push_back(num);
+                countMap[num]--;
+            }
+        }
+
+        return result;
+    }
+};
+ */
